52b97876cceb954d281306fc8fdf48640aba0804
deepaksenhacker <care.deepaksen@gmail.com>
2026-07-21 22:40:35 +0530

    update the array to add reduce filter map

--- feature-file.js
+++ feature-file.js
@@ -1,2 +1,5 @@
-const arr = [8,9,10,1,2,3,4,5];
-console.log(arr.sort((a,b)=>a-b));
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(arr.map((item)=>item*2));
+console.log(arr.reduce((prev,curr)=>prev+curr));
+console.log(arr.filter((item ,index)=>item >3 && item <7));
